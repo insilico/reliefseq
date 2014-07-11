13141045fba20574a23d657f7422b83b172ca143
Bill White <bill.c.white@gmail.com>
2014-07-11 13:48:01 -0500

    Removed all gomp and pthread references handled by -fopenmp flag. Was causing single thread operation.

--- config.h
+++ config.h
@@ -13,12 +13,6 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the `gomp' library (-lgomp). */
-#define HAVE_LIBGOMP 1
-
-/* Define to 1 if you have the `pthread' library (-lpthread). */
-#define HAVE_LIBPTHREAD 1
-
 /* Define to 1 if you have the <math.h> header file. */
 #define HAVE_MATH_H 1
 
